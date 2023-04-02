6db5d72b40f0fa14249c34f05288f451f6a28b9d
Killian Hedou <killian.hedou@gmail.com>
2023-04-01 23:23:26 -0400

    Fixed backpack glitch

--- RRAdventures/src/me/Geekenex/rradventures/items/Backpack.java
+++ RRAdventures/src/me/Geekenex/rradventures/items/Backpack.java
@@ -1,11 +1,14 @@
 package me.Geekenex.rradventures.items;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 import me.Geekenex.rradventures.Main;
 
@@ -21,8 +24,15 @@ public class Backpack implements Listener {
 	@EventHandler
 	public void bpOpen(PlayerInteractEvent e) {
 		Player p = e.getPlayer();
+		ItemStack item = e.getItem();
+		if(e.getAction() == null) return;
+		
+		if(e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+			if(item.getType().equals(Material.LIGHT_BLUE_DYE)) {
 		p.openInventory(p.getEnderChest());
 		p.playSound(p.getLocation(), Sound.ITEM_ARMOR_EQUIP_LEATHER, 1, 1);
+			}
+		}
 	}
-
+	
 }
